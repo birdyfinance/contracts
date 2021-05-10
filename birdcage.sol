e00e6a0b971b491a9a95c3453c31b3857496bd9c
Birdy.finance <83843364+birdyfinance@users.noreply.github.com>
2021-05-10 23:50:41 +0300

    added mint tl

--- birdcage.sol
+++ birdcage.sol
@@ -17,24 +17,31 @@ contract BirdCage {
     uint256 public PROPOSED_SET_POOL_allocPoint;
     uint16 public PROPOSED_SET_POOL_withdrawFee;
     uint256 public PROPOSED_EMISSION;
+    uint256 public PROPOSED_MINT_AMOUNT;
+    address public PROPOSED_MINT_TO;
+    address public PROPOSED_MINTER;
     
     //---------------------------------------------------------------
 
     uint public constant TIME_BEFORE_SET_POOL_EXECUTION = 12 hours;
     uint public constant TIME_BEFORE_ADD_POOL_EXECUTION = 12 hours;
     uint public constant TIME_BEFORE_EMISSION_CHANGE = 12 hours;
-    uint public constant TIME_BEFORE_OWNERSHIP_TRANSFER = 24 hours;
+    uint public constant TIME_BEFORE_MINT_TOKEN = 12 hours;
     
+    uint public constant TIME_BEFORE_OWNERSHIP_TRANSFER = 24 hours;
+    uint public constant TIME_BEFORE_MINTER_ROLE_TRANSFER = 24 hours;
     //---------------------------------------------------------------
     address public manager;
     MasterChefV2 public CHEF;
+    BirdToken public BIRD;
     address public pendingOwner;
-    enum Functions { transferOwnership, emergencyWithdraw, add, set, updateEmissionRate }
+    enum Functions { transferOwnership, emergencyWithdraw, add, set, updateEmissionRate, mintToken, transferMinter }
     mapping(Functions => uint) public timelock;
 
-    constructor(address _CHEF) public {
+    constructor(address _CHEF, address _birdToken) public {
         manager = msg.sender;
         CHEF = MasterChefV2(_CHEF);
+        BIRD = BirdToken(_birdToken);
     }
 
     // modifiers
@@ -83,4 +90,20 @@ contract BirdCage {
         CHEF.updateEmissionRate(PROPOSED_EMISSION);
     }
     
+    function proposeMintToken(address _to, uint256 _amount) external onlyManager setTimelock(Functions.mintToken, TIME_BEFORE_MINT_TOKEN) {
+        PROPOSED_MINT_AMOUNT = _amount;
+        PROPOSED_MINT_TO = _to;
+    }
+    
+    function executeMintToken() external enforceTimelock(Functions.mintToken) {
+        BIRD.mint(PROPOSED_MINT_TO, PROPOSED_MINT_AMOUNT);
+    }
+    
+    function proposeNewMinter(address newMinter) external onlyManager setTimelock(Functions.transferMinter, TIME_BEFORE_MINTER_ROLE_TRANSFER) {
+        PROPOSED_MINTER = newMinter;
+    }
+    
+    function executeNewMinter() external enforceTimelock(Functions.transferMinter) {
+        BIRD.setMinter(PROPOSED_MINTER);
+    }
 }
